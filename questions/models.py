8922bec975cd8126af7b8389680c160d99661c87
Suraj kumar <surajkumarsksk2000@gmail.com>
2025-01-24 08:43:52 +0530

    change in models.py file

--- questions/models.py
+++ questions/models.py
@@ -6,7 +6,7 @@ class Document(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name="Uploaded At")
     file_name = models.CharField(max_length=255, blank=True, verbose_name="File Name")
     
-    def _str_(self):
+    def __str__(self):
         return self.file_name if self.file_name else "Document " + str(self.id)
 
     class Meta:
